70a6110164f3d064f986c91cfe222c0de12fb405
camwoodward3 <camwoodward3@icloud.com>
2024-10-12 09:50:18 -0600

    Main Web update

--- fruit.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const fruitDropdown = document.getElementById('fruits');
-    const selectedFruitDisplay = document.getElementById('selectedFruit');
-
-    fruitDropdown.addEventListener('change', function() {
-        const selectedFruit = fruitDropdown.options[fruitDropdown.selectedIndex].text;
-        selectedFruitDisplay.textContext = `Selected fruit: ${selectedFruit}`;
-    });
-});
